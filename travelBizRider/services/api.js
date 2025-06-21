37a57bf93e4cec613a061ad7cc8145261a734eb3
Manish Matre <matremanish52@gmail.com>
2025-06-22 01:20:17 +0530

    login working proprly now

--- travelBizRider/services/api.js
+++ travelBizRider/services/api.js
@@ -1,5 +1,5 @@
 export async function loginDriver(email, password) {
-  const res = await fetch('http://localhost:5000/api/auth/login', {
+  const res = await fetch('http://192.168.67.77:5000/api/auth/login', {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify({ email, password })
